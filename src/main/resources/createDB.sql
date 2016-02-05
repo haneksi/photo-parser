d3650059cf2e6c922dd3f193d9d54a70529c5030
haneksi <seo.haneksi@gmail.com>
2016-02-05 14:46:17 +0300

    fix

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -2,22 +2,23 @@ create database if not exists photoparserdb;
 use photoparserdb;
 
 create table if not exists portfolio (
-  id int NOT NULL AUTO_INCREMENT ,
+    id int NOT NULL AUTO_INCREMENT ,
     url varchar(255) NOT NULL,
     author varchar(255) NOT NULL,
   primary key (id)
 );
 
 create table if not exists album(
-  id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     url varchar(255) NOT NULL,
     author varchar(255) NOT NULL references portfolio(author),
+    title varchar(255),
     portfolio_id int references portfolio(id),
   primary key(id)
 );
 
 create table if not exists image(
-  id int NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     url varchar(255) NOT NULL,
     author varchar(255) NOT NULL references portfolio(author),
     width int,
