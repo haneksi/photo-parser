01452cc1e1ec01554da7ac596b430f16ad7f62ca
haneksi <seo.haneksi@gmail.com>
2016-02-07 23:25:50 +0300

    fix

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -4,27 +4,40 @@ use photoparserdb;
 create table if not exists portfolio (
     id int NOT NULL AUTO_INCREMENT ,
     url varchar(255) NOT NULL,
-    author varchar(255) NOT NULL,
-  primary key (id)
-);
+    author varchar(255),
+    index(id),
+    index(author),
+    primary key (id)
+)ENGINE=InnoDB CHARACTER SET=UTF8;
 
 create table if not exists album(
     id int NOT NULL AUTO_INCREMENT,
     url varchar(255) NOT NULL,
-    author varchar(255) NOT NULL references portfolio(author),
+    author varchar(255),
     title varchar(255),
-    portfolio_id int references portfolio(id),
-  primary key(id)
-);
+    portfolio_id int,
+    index(portfolio_id),
+    index(author),
+    foreign key (author) references portfolio(author),
+    foreign key (portfolio_id) references portfolio(id),
+    primary key(id)
+)ENGINE=InnoDB CHARACTER SET=UTF8;
 
 create table if not exists image(
     id int NOT NULL AUTO_INCREMENT,
     url varchar(255) NOT NULL,
-    author varchar(255) NOT NULL references portfolio(author),
+    author varchar(255),
     width int,
     height int,
-    alt int,
-    portfolio_id int references portfolio(id),
-    album_id int references album(id),
-  primary key(id)
-);
+    alt varchar(255),
+    portfolio_id int,
+    album_id int,
+    index(portfolio_id),
+    index(album_id),
+    index(author),
+    foreign key(author) references portfolio(author),
+    foreign key(portfolio_id) references portfolio(id),
+    foreign key(album_id) references album(id),
+    primary key(id)
+)ENGINE=InnoDB CHARACTER SET=UTF8;
+
