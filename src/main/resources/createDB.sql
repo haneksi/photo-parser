a9148f5593e5cd93c83245e7bcc6c0bb98c337f8
haneksi <seo.haneksi@gmail.com>
2016-02-09 14:39:45 +0300

    fix script

--- src/main/resources/createDB.sql
+++ src/main/resources/createDB.sql
@@ -5,7 +5,7 @@ create table if not exists portfolio (
     portfolio_id int NOT NULL AUTO_INCREMENT,
     url varchar(255) NOT NULL,
     author varchar(255),
-    primary key(portfolio_id)
+primary key(portfolio_id)
 )ENGINE=InnoDB CHARACTER SET=UTF8;
 
 create table if not exists album(
@@ -13,13 +13,8 @@ create table if not exists album(
     url varchar(255) NOT NULL,
     author varchar(255),
     title varchar(255),
-    portfolio_id int not null,
-    CONSTRAINT album_portfolio_fk
-    FOREIGN KEY (portfolio_id)
-    REFERENCES portfolio (portfolio_id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    primary key(album_id)
+    portfolio_id int not null references portfolio(portfolio_id),
+primary key(album_id)
 )ENGINE=InnoDB CHARACTER SET=UTF8;
 
 create table if not exists image(
@@ -29,17 +24,7 @@ create table if not exists image(
     width int,
     height int,
     alt varchar(255),
-    portfolio_id int not null,
-    album_id int not null,
-    CONSTRAINT image_portfolio_fk
-    FOREIGN KEY (portfolio_id)
-    REFERENCES portfolio (portfolio_id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT image_album_fk
-    FOREIGN KEY (album_id)
-    REFERENCES album (album_id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    primary key(image_id)
+    portfolio_id int not null references portfolio(portfolio_id),
+    album_id int not null references album(album_id),
+primary key(image_id)
 )ENGINE=InnoDB CHARACTER SET=UTF8;
