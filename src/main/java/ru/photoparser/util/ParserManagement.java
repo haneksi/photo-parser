54be7ac5734cd50e21526d0343222a16f27f9b54
haneksi <seo.haneksi@gmail.com>
2016-02-12 14:33:32 +0300

    add method getImagesToAlbumWP

--- src/main/java/ru/photoparser/util/ParserManagement.java
+++ src/main/java/ru/photoparser/util/ParserManagement.java
@@ -3,9 +3,16 @@ package ru.photoparser.util;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.photoparser.entity.Album;
+import ru.photoparser.entity.Image;
+import ru.photoparser.entity.Portfolio;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParserManagement {
 
@@ -24,9 +31,49 @@ public class ParserManagement {
         return doc;
     }
 
-//    public static Object getBean(String beanName, Class clazz){
-//        return new ClassPathXmlApplicationContext(new String[] {"context.xml"}).getBean(beanName, clazz);
-//    }
 
+    public static List<Image> getImagesToAlbumWP(Album album, Portfolio portfolio, String author){
+        Document doc = ParserManagement.getDocument(album.getUrl());
+        List<Image> listImages = new ArrayList<Image>();
 
+        Elements linkImages = doc.select("div#content").get(0).select("img[src$=.jpg]");
+        for (Element linkImage : linkImages) {
+            String url = linkImage.attr("src");
+            String width = linkImage.attr("width");
+            String height = linkImage.attr("height");
+            String alt = linkImage.attr("alt");
+            Image image = new Image();
+            image.setAuthor(author);
+            image.setHeight(height);
+            image.setWidth(width);
+            image.setUrl(url);
+            image.setAlt(alt);
+            image.setPortfolio(portfolio);
+            image.setAlbum(album);
+
+            listImages.add(image);
+        }
+
+        linkImages = doc.getElementsByAttribute("data-lazyload-src");
+        for (Element element : linkImages){
+            String link = element.attr("data-lazyload-src");
+            String width = element.attr("width");
+            String height = element.attr("height");
+            String alt = element.attr("alt");
+            if(link.endsWith(".jpg")) {
+                Image image = new Image();
+                image.setAlbum(album);
+                image.setPortfolio(portfolio);
+                image.setUrl(link);
+                image.setWidth(width);
+                image.setHeight(height);
+                image.setAlt(alt);
+                image.setAuthor(author);
+
+                listImages.add(image);
+            }
+        }
+
+        return listImages;
+    }
 }
