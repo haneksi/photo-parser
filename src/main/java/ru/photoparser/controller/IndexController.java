99c9a71064748dc3f423f85ae446975ef29f53b0
haneksi <seo.haneksi@gmail.com>
2016-02-25 14:55:28 +0300

    fix

--- src/main/java/ru/photoparser/controller/IndexController.java
+++ src/main/java/ru/photoparser/controller/IndexController.java
@@ -7,11 +7,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.photoparser.entity.Portfolio;
-import ru.photoparser.parse.Parser;
-import ru.photoparser.parse.impl.ErichmcveyParser;
 import ru.photoparser.service.PortfolioService;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Controller
@@ -21,29 +18,11 @@ public class IndexController {
     @Autowired
     private PortfolioService portfolioService;
 
-//    @Qualifier("tinydotphotographyParser")
-//    @Autowired
-//    private Parser parser;
-
-    @Resource(name = "parserCollection")
-    private List<Parser> list;
-
-
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        for (Parser parser : list) {
-            Portfolio portfolio = parser.parsing();
-            portfolioService.create(portfolio);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-//        Portfolio parsing = parser.parsing();
-//        portfolioService.create(parsing);
-
+        List<Portfolio> portfolioList = portfolioService.getAll();
+        model.addAttribute("portfolioList", portfolioList);
         return "index";
     }
 }
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -1,20 +1,26 @@
 
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>PhotoParser</title>
+    <script src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
+    <script src="/js/left-menu.js" type="text/javascript"></script>
+    <link href="/css/left-menu.css" rel="stylesheet"/>
 </head>
 <body>
-<div class = "photo" align="center" style="background-color: black">
-    <c:forEach var="image" items="${images}" varStatus="varCount" end="20">
-        <img src="${image.url}" height="200" />
-        <%--<c:if test="${varCount.count % 5 == 0}">--%>
-            <%--<br/>--%>
-        <%--</c:if>--%>
-    </c:forEach>
-</div>
+    <div id="left-menu">
+        <c:forEach items="${portfolioList}" var="portfolio">
 
+            ${portfolio.url}
 
+            <c:forEach items="${portfolio.albums}" var="album">
+                <div id="item-left-menu">
+                    ${album.title}
+                </div>
+            </c:forEach>
+
+        </c:forEach>
+    </div>
 </body>
 </html>
