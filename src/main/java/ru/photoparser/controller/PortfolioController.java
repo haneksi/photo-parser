2af689b89bdbbccf1d1e390d2c2afb880a8f3d26
haneksi <seo.haneksi@gmail.com>
2016-02-28 23:17:24 +0300

    impl

--- src/main/java/ru/photoparser/controller/PortfolioController.java
+++ src/main/java/ru/photoparser/controller/PortfolioController.java
@@ -1,8 +1,33 @@
 package ru.photoparser.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import ru.photoparser.entity.Portfolio;
+import ru.photoparser.service.PortfolioService;
 
+import java.util.List;
+
+@SessionAttributes(value = "portfolioList")
 @Controller
 public class PortfolioController {
+    @Qualifier("portfolioService")
+    @Autowired
+    private PortfolioService portfolioService;
+
+
+    @RequestMapping(value = "/portfolio_collection", method = RequestMethod.GET)
+    public String getAllCollection(ModelMap model) {
+
+        List<Portfolio> portfolioList = portfolioService.getAll();
+        if(portfolioList != null) {
+            model.addAttribute("portfolioList", portfolioList);
+        }
+        return "index";
+    }
 
 }
